233f4d13548e7890ca7dba38d495c6dbf0d4aa03
architKumar <https://github.com/architdhariwal>
2024-11-21 17:52:28 +0530

    corrected API_URL

--- Nov-21-2024-Task-Solutions/react-assignment/src/services/apiClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_JSON_SERVER,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
--- Nov-21-2024-Task-Solutions/react-assignment/src/services/customerService.ts
+++ Nov-21-2024-Task-Solutions/react-assignment/src/services/customerService.ts
@@ -1,7 +1,7 @@
 import { Customer } from '../types/Customer';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_JSON_SERVER;
 
 export const customerService = {
   getAllCustomers: async () => {
